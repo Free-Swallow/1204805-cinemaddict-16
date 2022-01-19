7a54cb433d3c4aa94f1e125e3fcb27e6d7ace240
Nicolay Egorov <nickphoenix21@icloud.com>
2022-01-19 23:40:41 +0700

    Ломанная сортировка

--- src/view/sort-list-view.js
+++ src/view/sort-list-view.js
@@ -1,10 +1,16 @@
 import AbstractView from './abstract-view.js';
 
+const SortType = {
+  DEFAULT: 'default',
+  DATE: 'releaseYear',
+  RATING: 'rating',
+};
+
 const createSortListTemplate = () => (
   `<ul class="sort">
-   <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-   <li><a href="#" class="sort__button">Sort by date</a></li>
-   <li><a href="#" class="sort__button">Sort by rating</a></li>
+   <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+   <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+   <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
    </ul>`
 );
 
@@ -12,6 +18,21 @@ class SortListView extends AbstractView {
   get template() {
     return createSortListTemplate();
   }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  }
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'A') {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
 }
 
 export default SortListView;
+export {SortType};
